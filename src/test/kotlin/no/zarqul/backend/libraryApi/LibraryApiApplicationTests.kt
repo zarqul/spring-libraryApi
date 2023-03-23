f4f0e42e6b17ad891b791373316518fab06bd6db
jjo <jjo@isys.no>
2023-03-23 13:36:19 +0100

    Added Spring/Kotlin project

---
+++ src/test/kotlin/no/zarqul/backend/libraryApi/LibraryApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.zarqul.backend.libraryApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
